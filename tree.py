7718ac00136920582f3bf467c5bff35f8ff7edd1
Jayanthimohan <32294892+Jayanthimohan@users.noreply.github.com>
2017-10-24 11:02:20 +0530

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,16 @@
+import redis
+r=redis.Redis(host='localhost',port=6379,db=0)
+# d = {'0':123, '1':'345', '2':'678'}
+# print(type(d['0']))
+
+r.hmset('tree',{'0':'123', '1':'345', '2':'678'})
+b=r.hgetall('tree')
+print(b)
+# print(type(b))
+# print(type(b['0']))
+print (b['0'])
+
+r.hmset('nodes',{'img_id':'abc.jpg','id':'1'})
+c=r.hgetall('nodes')
+print(c)
+print (c['id'])
